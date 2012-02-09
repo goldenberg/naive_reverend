5ea9222c6d62a35944d9abd722597beb982286fd
Benjamin Goldenberg <bgoldenberg@gmail.com>
2012-02-09 01:31:20 -0800

    everythign compiles, but one class always wins

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 	var correct, wrong uint
 	for d := range evalData {
 		class, p := nb.Classify(d.Features)
-		fmt.Println("Was:", d.Class, "Got:", class, "With p:", p)
+		fmt.Println("Was:", d.Class, "Got:", class)//, "With p:", p)
 		if class == d.Class {
 			correct += 1
 		} else {

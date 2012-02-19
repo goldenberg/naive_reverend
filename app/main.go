ffe95ae925c01c29837416085e1d09f2819f997b
Benjamin Goldenberg <bgoldenberg@gmail.com>
2012-02-18 22:24:12 -0800

    Add IncrN and status handler

--- app/main.go
+++ app/main.go
@@ -25,7 +25,7 @@ func main() {
 	quitServer := make(chan bool)
 
 	trainData := make(chan *model.Datum, 100)
-	evalData := make(chan *model.Datum, 400)
+	evalData := make(chan *model.Datum, 400000)
 
 	nb := model.New()
 
@@ -67,11 +67,10 @@ func main() {
 }
 
 func Serve(nb *model.NaiveBayes, quit chan bool) {
-	var cHandler ClassifyHandler
-	cHandler.nb = nb
-
+	fmt.Println("serving")
 	http.HandleFunc("/hello", HelloServer)
-	http.Handle("/classify", cHandler)
+	http.Handle("/status", StatusHandler{nb})
+	http.Handle("/classify", ClassifyHandler{nb})
 
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
@@ -112,6 +111,19 @@ func (h ClassifyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	return
 }
 
+type StatusHandler struct {
+	nb *model.NaiveBayes
+}
+
+func (h StatusHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	jsonWriter := json.NewEncoder(w)
+	jsonWriter.Encode(map[string]interface{}{
+		"prior": h.nb.ClassCounter.String(),
+		"num_features": len(h.nb.FeatureCategoryCounters),
+	})
+	return
+}
+
 func ReadData(reader io.Reader, out chan *model.Datum, quit chan bool) {
 	jsonDecoder := json.NewDecoder(reader)
 	i := 0
@@ -121,6 +133,9 @@ func ReadData(reader io.Reader, out chan *model.Datum, quit chan bool) {
 		if err != nil {
 			fmt.Println(err)
 			break
+		}
+		if x.Count == 0 {
+			x.Count = 1
 		}
 		out <- &x
 		i += 1
